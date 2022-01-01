971c2915b1dcc1b346b8ba17f1bc547ff58ea5d5
wadr2009 <gz_dengwei@163.com>
2022-01-01 15:04:02 +0800

    配置修改

--- anymock_opensource.sql
+++ anymock_opensource.sql
@@ -25,18 +25,18 @@ CREATE TABLE `anymock_http_interface`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `request_uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `request_method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `need_async_callback` tinyint(1) NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL comment '描叙',
+  `need_async_callback` tinyint(1) NOT NULL comment '是否需要异步回调',
   `config_mode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `response_body` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `callback_request_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `callback_request_method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `callback_request_body` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `branch_jump_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `sync_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `async_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `sync_delay` int(11) NOT NULL,
-  `async_delay` int(11) NOT NULL,
+  `sync_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL comment '同步脚本',
+  `async_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL comment '异步脚本',
+  `sync_delay` int(11) NOT NULL comment '同步延迟',
+  `async_delay` int(11) NOT NULL comment '异步延迟',
   `is_start` tinyint(1) NOT NULL,
   `space_id` bigint(20) NOT NULL,
   `last_update_user` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE `anymock_http_interface`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_request_mapping`(`request_uri`, `request_method`) USING BTREE,
   INDEX `idx_space_id`(`space_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact comment 'anymockhttp接口';
 
 -- ----------------------------
 -- Table structure for anymock_interface_branch
@@ -55,7 +55,7 @@ CREATE TABLE `anymock_http_interface`  (
 DROP TABLE IF EXISTS `anymock_interface_branch`;
 CREATE TABLE `anymock_interface_branch`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `interface_id` bigint(20) NOT NULL,
+  `http_interface_id` bigint(20) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sync_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `async_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
