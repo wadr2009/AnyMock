f1f927fd953515d8ffe8e69aac558ea34cf15e48
wadr2009 <gz_dengwei@163.com>
2022-01-09 21:35:52 +0800

    重启kill老服务

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+port=`ps -ef |grep anymock | awk '{print $2}'`
+
+if ((${#port[*]} > 2 ))
+then
+  kill -9 ${port[0]}
+  kill -9 ${port[1]}
+fi
 
 cd ~/AnyMock/output/core
 nohup java -jar anymock-core-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
