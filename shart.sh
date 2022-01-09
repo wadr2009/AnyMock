f2a6dcf2f71aa9bcf7310964aace290e1ff6fce2
wadr2009 <gz_dengwei@163.com>
2022-01-09 16:29:05 +0800

    启动修复

--- shart.sh
+++
@@ -1,5 +0,0 @@
-
-
-nohup java -jar anymock-core-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
-sleep 5
-nohup java -jar anymock-manager-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
---
+++ start.sh
@@ -0,0 +1,6 @@
+cd ~/AnyMock/output/core
+nohup java -jar anymock-core-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
+sleep 5
+
+cd ~/AnyMock/output/manager
+nohup java -jar anymock-manager-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
