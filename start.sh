13530d3666aeabb3f12c2bb9be572223c9999501
wadr2009 <gz_dengwei@163.com>
2022-01-09 22:42:36 +0800

    换内网ip

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ nohup java -jar anymock-core-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
 sleep 5
 
 cd ~/AnyMock/output/manager
-nohup java -jar anymock-manager-runner-1.0-SNAPSHOT.jar >info.log 2>error.log &
+nohup java -jar anymock-manager-runner-1.0-SNAPSHOT.jar -Djava.net.preferIPv4Stack=true >info.log 2>error.log &
